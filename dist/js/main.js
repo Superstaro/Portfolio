1c9bc97120a27b2a3ea39f4dbc7820c6a4431d9b
superstaro <victorbrown33@hotmail.com>
2019-06-04 12:58:17 -0400

    remove menu-branding side and updated footer in portfolio

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
 const navItem = document.querySelectorAll(".nav-item");
 
 //Set initial state of menu
@@ -15,7 +14,6 @@ function toggleMenu() {
     menuBtn.classList.add("close");
     menu.classList.add("show");
     menuNav.classList.add("show");
-    menuBranding.classList.add("show");
     navItem.forEach(item => item.classList.add("show"));
 
     //Reset Menu State
@@ -24,7 +22,6 @@ function toggleMenu() {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
     navItem.forEach(item => item.classList.remove("show"));
 
     //Reset Menu State
